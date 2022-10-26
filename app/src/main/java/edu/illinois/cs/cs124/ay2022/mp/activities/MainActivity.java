48cfbbc5bffd5a7f0a0aa1d92fcfff84c8133bd7
elhesh1 <ehs4@illinoi.edu>
2022-10-25 21:39:09 -0500

    Did MP0. Added ID to ID.text, set location center and changed name

--- app/src/main/java/edu/illinois/cs/cs124/ay2022/mp/activities/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs124/ay2022/mp/activities/MainActivity.java
@@ -71,7 +71,7 @@ public final class MainActivity extends AppCompatActivity
 
     // Load the layout for this activity and set the title
     setContentView(R.layout.activity_main);
-    setTitle("Favorite Place");
+    setTitle("Favorite Places");
 
     // Find the MapView component in the layout and configure it properly
     // Also save the reference for later use
@@ -95,6 +95,7 @@ public final class MainActivity extends AppCompatActivity
     // Set the current map zoom level to the default
     IMapController mapController = mapView.getController();
     mapController.setZoom(MAP_DEFAULT_ZOOM);
+    mapController.setCenter(new GeoPoint(40.10986682167534, -88.22831928981661));
   }
 
   /*
