8b55da6fd022aadc1cb62cd8d3d9d2abfc784386
elhesh1 <ehs4@illinoi.edu>
2022-11-17 21:26:41 -0600

    Pre MP2 With MP2 tests first MP2 test - with lint - with yaml- test 0-2 MP2

--- app/src/main/java/edu/illinois/cs/cs124/ay2022/mp/activities/AddPlaceActivity.java
+++ app/src/main/java/edu/illinois/cs/cs124/ay2022/mp/activities/AddPlaceActivity.java
@@ -1,5 +1,60 @@
 package edu.illinois.cs.cs124.ay2022.mp.activities;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import edu.illinois.cs.cs124.ay2022.mp.R;
+import edu.illinois.cs.cs124.ay2022.mp.models.Place;
+import edu.illinois.cs.cs124.ay2022.mp.models.ResultMightThrow;
+import edu.illinois.cs.cs124.ay2022.mp.network.Client;
+import java.util.function.Consumer;
 
-public class AddPlaceActivity extends AppCompatActivity {}
+public class AddPlaceActivity extends AppCompatActivity {
+  private static final String TAG = AddPlaceActivity.class.getSimpleName();
+  private Consumer<ResultMightThrow<Boolean>> callback;
+
+  @Override
+  public void onCreate(@Nullable final Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_addplace);
+
+    // Load the layout for this activity and set the title
+
+    Intent returnToMain = new Intent(this, MainActivity.class);
+    returnToMain.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+
+    Button cancelButton = findViewById(R.id.cancel_button);
+    cancelButton.setOnClickListener(
+        v -> {
+          startActivity(returnToMain);
+        });
+// iiiiiiii
+    Intent savePlace = new Intent(this, Place.class);
+    Button saveButton = findViewById(R.id.save_button);
+    saveButton.setOnClickListener(
+        v -> {
+          Bundle location = getIntent().getExtras();
+          ;;;
+          Double mylong = null;
+          Double mylat = null;
+          if (location != null) {
+            String thislong = location.getString("longitude");
+            String thislat = location.getString("latitude");
+            mylong = Double.parseDouble(thislong);
+            mylat = Double.parseDouble(thislat);
+          }
+
+          EditText desc = findViewById(R.id.description);
+          String mydesc = desc.getText().toString();
+
+          Place myplace =
+              new Place("2ec94fdc-60c5-4fe1-8c83-7c43fcf43131", "Elliot", mylat, mylong, mydesc);
+
+          Client.start().postFavoritePlace(myplace, callback);
+          startActivity(returnToMain);
+        });
+  }
+}
--- app/src/main/java/edu/illinois/cs/cs124/ay2022/mp/application/FavoritePlacesApplication.java
+++ app/src/main/java/edu/illinois/cs/cs124/ay2022/mp/application/FavoritePlacesApplication.java
@@ -24,7 +24,7 @@ public final class FavoritePlacesApplication extends Application {
   public static final String SERVER_URL = "http://localhost:" + DEFAULT_SERVER_PORT + "/";
 
   // Put your ID (from ID.txt) here
-  public static final String CLIENT_ID = "";
+  public static final String CLIENT_ID = "2ec94fdc-60c5-4fe1-8c83-7c43fcf43131";
 
   // Reference to our API client, which would potentially be used by multiple app activities
   private Client client;
